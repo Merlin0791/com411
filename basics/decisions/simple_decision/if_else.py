a87e336d6ba2e7f9947f07cefd6351839beda1fe
repl.it user <replituser@example.com>
2020-10-06 10:21:09 +0000

    added code to show else if

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,9 @@
+print("Please enter the activity to be performed.")
+activity=input()
+
+if (activity == "Calculate" or "calculate"):
+  print("\n performing calculations....")
+else:
+  print("\n Performing Activity...")
+
+print("\n Activity completed.")
