9be018cee188a9e194d63ebff1386a4c66ff920a
repl.it user <replituser@example.com>
2020-10-06 10:40:30 +0000

    added code to show elif statements

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,15 @@
+
+
+print("\nTowards with direction should I paint (up, down, left or right")
+direction = input()
+
+if (direction == "up"):
+  print("\ni am painting in the upward direction!")
+elif (direction == "down"):
+  print("\ni am painting in the downward direction!")
+elif (direction == "left"):
+  print("\ni am painting in the leftward direction!")
+elif (direction == "right"):
+  print("\ni am painting in the rightward direction!")
+else:
+  print("\nnot sure what direction to paint!!!")
