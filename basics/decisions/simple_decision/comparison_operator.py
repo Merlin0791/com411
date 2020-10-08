f70260bd591160615d884c4a6a57156b7f79a968
repl.it user <replituser@example.com>
2020-10-08 12:48:55 +0000

    added code about Comparsion Operators

---
+++ basics/decisions/simple_decision/comparison_operator.py
@@ -0,0 +1,6 @@
+
+print("Please enter the first number")
+firstnumber = int(input())
+
+print("Please enter the second number")
+secondnumber = int(input())
---
+++ basics/input/user_input.py
@@ -0,0 +1,3 @@
+print("\n What is your name human?")
+name= input()
+print("It's nice to meet you {} .".format(name))
