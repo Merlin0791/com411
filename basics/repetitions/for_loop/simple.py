5dc6ed17fdceeb969f782b5ae20554df13ca7d14
repl.it user <replituser@example.com>
2020-10-17 21:49:12 +0000

    nesting loops

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,18 @@
+# Ask user for number of mountainsprint
+("How many mountains should I display?")
+mountains = int(input())
+
+# Display mountains
+print("\nDisplaying...")
+
+for mountains in range(mountains):
+  print("""
+            __
+           /  \\_  
+           /^    \\
+          /  ^    \\_
+       _/ ^ ^     ^\\
+      /  ^     ^    \\
+
+
+      """)
