22c402bd568273d51523de72a7f81c838e8a2bc2
repl.it user <replituser@example.com>
2020-10-17 23:03:26 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,12 @@
+def run():
+
+  print("\nWhat lies ahead?\n")
+  object = input()
+
+  if (object == "Monster"):
+    print("\nIt's time to RUN!!!!\n")
+  else:
+    print("\nLets keep going\n")
+
+
+run()
---
+++ basics/functions/simple_function.py
@@ -0,0 +1,10 @@
+def listen():
+
+
+  print("\nWhat sound did I hear?\n")
+  sound = input()
+
+  print("\nThat was a loud {} It made me jump" .format(sound))
+
+
+listen()
