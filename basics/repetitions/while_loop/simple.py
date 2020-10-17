e4d7e662159b7cedfcc5ece72460204e9cc09820
repl.it user <replituser@example.com>
2020-10-17 20:17:04 +0000

    Simple While loop made

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,10 @@
+print("How amny cables shall i remove?")
+cable_to_remove = int(input())
+
+time_to_remove = 0
+
+print("starting to remove the cables")
+
+while (cable_to_remove > time_to_remove):
+  print("removed cable...")
+  time_to_remove = time_to_remove + 1
