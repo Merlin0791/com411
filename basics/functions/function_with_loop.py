f90c7b98a0addd3d56c3435f4164636379aab53e
repl.it user <replituser@example.com>
2020-10-18 01:01:21 +0000

    Added code to demonstrate the use of a user-defined function with multiple parameters

---
+++ basics/functions/function_with_loop.py
@@ -0,0 +1,12 @@
+def crossed_brige(steps):
+
+  for step in range(steps):
+    print("Crossed step.")
+
+
+  if (steps > 5):
+     print("The bridge is collapsing!")
+  else:
+     print ("We must keep going!")
+
+crossed_brige(2)
